f8910560f67caa8f4917286c0db5edbf7dcd7fb3
lukaschen <villagelukaschen@gmail.com>
2016-03-02 07:33:26 +0000

    update

---
+++ js/controllers/videogular.js
@@ -0,0 +1,12 @@
+angular.module('videoDemo')
+.controller('videoCtrl',function($scope,$sce) {
+   var vm = this;
+   
+   vm.config = {
+       sources: [
+            {src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.mp4"), type: "video/mp4"},
+			   {src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.webm"), type: "video/webm"},
+		      {src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.ogg"), type: "video/ogg"}
+        ],
+   }
+});
